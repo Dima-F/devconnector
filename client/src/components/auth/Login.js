75d521c8e1255661a8bc21c349b30a75b6b5a5b6
Dima-f <dafw@live.ru>
2018-07-10 23:48:51 +0300

    input groups

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -48,7 +48,7 @@ class Login extends Component {
               <div className="container">
                 <div className="row">
                   <div className="col-md-8 m-auto">
-        true            <h1 className="display-4 text-center">Log In</h1>
+                    <h1 className="display-4 text-center">Log In</h1>
                     <p className="lead text-center">Sign in to your DevConnector account</p>
                     <form onSubmit ={this.onSubmit}>
                         <TextFieldGroup
