eae8cc08cd45c433b9e9d464eff8f87c4ad66e89
Dima-f <dafw@live.ru>
2018-07-21 00:17:13 +0300

    add experience compnent

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -43,6 +43,17 @@ export const deleteAccount = ()=> dispatch => {
             );
     }
 }
+export const addExperience = (expData, history)=>dispatch=>{
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res=>history.push('/dashboard'))
+        .catch(err=>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
 
 export const setProfileLoading = () =>  {
     return {
