9431717b7ba0f459be7fa7f63ac387b94eddd5cc
Dima-f <dafw@live.ru>
2018-07-26 23:32:00 +0300

    profile completed

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import PrivateRoute from './components/common/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 import store from './store';
 
 import './App.css';
@@ -48,6 +49,7 @@ class App extends Component {
                         <Route exact path="/login" component={ Login } />
                         <Route exact path="/profiles" component={ Profiles } />
                         <Route exact path="/profile/:handle" component={ Profile } />
+                        <Route exact path="/not-found" component={ NotFound } />
                         <Switch>
                             <PrivateRoute exact path="/dashboard" component={ Dashboard } />
                         </Switch>
